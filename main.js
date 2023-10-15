99322b942a4b1c503ebeadac8ed448ccd3c762c4
Callisto <97772639+Zeuesser@users.noreply.github.com>
2023-10-16 02:01:22 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 //base by DGXeon (Xeon Bot Inc.)
-//YouTube: @saadibrahim_900
-//Instagram: Saadibrahim900
-//Telegram: t.me/saad143s
-//GitHub: @saad143ss
-//WhatsApp: +923156157942
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@saadibrahim_900
+//YouTube: @Callisto-god
+//Instagram: tf.joe7
+//Telegram: t.me/secondtate
+//GitHub: @zeuesser
+//WhatsApp: +9188916677340
 
 require('./settings')
 const pino = require('pino')
